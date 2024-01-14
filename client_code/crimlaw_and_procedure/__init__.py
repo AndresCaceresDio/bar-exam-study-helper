632131cc3c74c9bde84fdee8b9137e1a3e24b5da
AndresCaceresDio <156248774+AndresCaceresDio@users.noreply.github.com>
2024-01-13 20:42:45 -0500

    Delete client_code directory

--- client_code/crimlaw_and_procedure/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import crimlaw_and_procedureTemplate
-from anvil import *
-
-class crimlaw_and_procedure(crimlaw_and_procedureTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
--- client_code/evidence/__init__.py
+++
@@ -1,9 +0,0 @@
-from ._anvil_designer import evidenceTemplate
-from anvil import *
-
-class evidence(evidenceTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
